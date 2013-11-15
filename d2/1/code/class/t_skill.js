469c1d27c23ffc333a6bbf4a9f0bb432543163bb
hellopacman <hellopacman@hotmail.com>
2013-11-16 00:47:49 +0800

    日常

---
+++ d2/1/code/class/t_skill.js
@@ -0,0 +1,13 @@
+
+/*********************************
+		道具模板数据
+***********************************/
+//-- 技能模板表
+var T_SKILL_LIST = new Object();
+var i = 0;			//自增索引
+
+//pacman 2013-11-6
+//近战武器攻击	使用当前武器进行一次物理攻击
+T_SKILL_LIST[i] = new Object();
+T_SKILL_LIST[i].TID = i; 					//技能TID
+T_SKILL_LIST[i].NAME = "Attack"; 			//技能名称
